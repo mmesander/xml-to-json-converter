5805ae9d247a543409292c10d90ff2af1cd10453
Mark Mesander <mark_mesander@hotmail.com>
2025-01-11 16:53:56 +0100

    add tests for xmlToJava and javaToJson

--- src/test/java/nl/mesander/services/EmployeeServiceTest.java
+++ src/test/java/nl/mesander/services/EmployeeServiceTest.java
@@ -12,19 +12,35 @@ class EmployeeServiceTest {
     private EmployeeService employeeService;
 
     EmployeeInputDto mockEmployeeInputDto;
+    EmployeeDto mockEmployeeDto;
+    String testFileLocation;
+    String mockJsonString;
 
     @BeforeEach
     void init() {
         mockEmployeeInputDto = new EmployeeInputDto();
         mockEmployeeInputDto.setName("Mark Test");
-        mockEmployeeInputDto.setFunction("Java Developer Test");
+        mockEmployeeInputDto.setFunction("Junior Java Developer Test");
         mockEmployeeInputDto.setCompany("Copernicus Test");
         mockEmployeeInputDto.setToHire("Absolutely");
+
+        testFileLocation = "src/test/resources/EmployeeTest.xml";
+
+        mockJsonString = "{\"name\":\"Mark Super Test\",\"function\":\"Medior Java Developer Test\",\"company\":\"Copernicus Super Test\",\"isHired\":\"100% Yes\"}";
+
+        mockEmployeeDto = new EmployeeDto();
+        mockEmployeeDto.setName("Mark Super Test");
+        mockEmployeeDto.setFunction("Medior Java Developer Test");
+        mockEmployeeDto.setCompany("Copernicus Super Test");
+        mockEmployeeDto.setIsHired("100% Yes");
     }
 
     @AfterEach
     void tearDown() {
         mockEmployeeInputDto = null;
+        mockEmployeeDto = null;
+        testFileLocation = "";
+        mockJsonString = "";
     }
 
     @Test
@@ -44,22 +60,33 @@ class EmployeeServiceTest {
     }
 
     @Test
+    @DisplayName("Should transfer XML file to EmployeeInputDto")
     void xmlToJava() {
         // Arrange
+        // BeforeEach init String: testFileLocation
 
         // Act
+        EmployeeInputDto result = EmployeeService.xmlToJava(testFileLocation);
 
         // Assert
-
+        assertEquals("Mark Mesander Test", result.getName());
+        assertEquals("Junior Java Developer Test", result.getFunction());
+        assertEquals("Copernicus Test", result.getCompany());
+        assertEquals("Absolutely", result.getToHire());
     }
 
     @Test
+    @DisplayName("Should transfer EmployeeDto to JsonString")
     void javaToJson() {
         // Arrange
+        // BeforeEach init EmployeeDto: mockEmployeeDto
+        // BeforeEach init String: mockJsonString
 
         // Act
+        String result = EmployeeService.javaToJson(mockEmployeeDto);
 
         // Assert
+        assertEquals(mockJsonString, result);
 
     }
 }

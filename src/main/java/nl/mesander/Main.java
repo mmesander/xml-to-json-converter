28e3271e0a63daacaa916458664c5efdda16b8cf
Mark Mesander <mark_mesander@hotmail.com>
2025-01-11 15:23:23 +0100

    add prints for both methods in main and fix problem with xml not being read

--- src/main/java/nl/mesander/Main.java
+++ src/main/java/nl/mesander/Main.java
@@ -1,6 +1,7 @@
 package nl.mesander;
 
 import nl.mesander.dtos.input.EmployeeInputDto;
+import nl.mesander.dtos.output.EmployeeDto;
 
 import static nl.mesander.services.EmployeeService.*;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -9,9 +10,30 @@ public class Main {
     public static void main(String[] args) {
         String fileLocation = "src/main/resources/Employee.xml";
         EmployeeInputDto employeeInputDto = xmlToJava(fileLocation);
-        System.out.println(employeeInputDto.getName());
-        System.out.println(employeeInputDto.getFunction());
-        System.out.println(employeeInputDto.getCompany());
-        System.out.println(employeeInputDto.getToHire());
+
+        if (employeeInputDto != null) {
+            System.out.println("XML naar Java: ");
+            System.out.println(employeeInputDto.getName());
+            System.out.println(employeeInputDto.getFunction());
+            System.out.println(employeeInputDto.getCompany());
+            System.out.println(employeeInputDto.getToHire());
+            System.out.println("------------------");
+            System.out.println(" ");
+        } else {
+            System.out.println("Employee not found for transfer xml to java");
+        }
+
+        if (employeeInputDto != null) {
+            EmployeeDto employeeDto = employeeToDto(employeeInputDto);
+            System.out.println("Input naar output: ");
+            System.out.println(employeeDto.getName());
+            System.out.println(employeeDto.getFunction());
+            System.out.println(employeeDto.getCompany());
+            System.out.println(employeeDto.getIsHired());
+        } else {
+            System.out.println("Employee not found for transfer input to output");
+        }
+
+
     }
 }

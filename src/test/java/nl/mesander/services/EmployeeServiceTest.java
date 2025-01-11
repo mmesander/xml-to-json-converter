fbd0e3592ab8f85bbac2c92bf1cbfe7f92db36d6
Mark Mesander <mark_mesander@hotmail.com>
2025-01-11 16:00:40 +0100

    add empty tests

---
+++ src/test/java/nl/mesander/services/EmployeeServiceTest.java
@@ -0,0 +1,16 @@
+package nl.mesander.services;
+
+import junit.framework.TestCase;
+
+public class EmployeeServiceTest extends TestCase {
+
+
+    public void testEmployeeToDto() {
+    }
+
+    public void testXmlToJava() {
+    }
+
+    public void testJavaToJson() {
+    }
+}

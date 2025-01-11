aeda19c591b39c586bf901525f2071ae76e83968
Mark Mesander <mark_mesander@hotmail.com>
2025-01-11 16:26:46 +0100

    add test for employeeToDto

--- src/test/java/nl/mesander/services/EmployeeServiceTest.java
+++ src/test/java/nl/mesander/services/EmployeeServiceTest.java
@@ -1,16 +1,65 @@
 package nl.mesander.services;
 
-import junit.framework.TestCase;
+import nl.mesander.dtos.input.EmployeeInputDto;
+import nl.mesander.dtos.output.EmployeeDto;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class EmployeeServiceTest extends TestCase {
+class EmployeeServiceTest {
+    private EmployeeService employeeService;
 
+    EmployeeInputDto mockEmployeeInputDto;
 
-    public void testEmployeeToDto() {
+    @BeforeEach
+    void init() {
+        mockEmployeeInputDto = new EmployeeInputDto();
+        mockEmployeeInputDto.setName("Mark Test");
+        mockEmployeeInputDto.setFunction("Java Developer Test");
+        mockEmployeeInputDto.setCompany("Copernicus Test");
+        mockEmployeeInputDto.setToHire("Absolutely");
     }
 
-    public void testXmlToJava() {
+    @AfterEach
+    void tearDown() {
+        mockEmployeeInputDto = null;
     }
 
-    public void testJavaToJson() {
+    @Test
+    @DisplayName("Should transfer EmployeeInputDto to EmployeeDto")
+    void employeeToDto() {
+        // Arrange
+        // BeforeEach init EmployeeInputDto: mockEmployeeInputDto
+
+        // Act
+        EmployeeDto result = EmployeeService.employeeToDto(mockEmployeeInputDto);
+
+        // Assert
+        assertEquals(mockEmployeeInputDto.getName(), result.getName());
+        assertEquals(mockEmployeeInputDto.getFunction(), result.getFunction());
+        assertEquals(mockEmployeeInputDto.getCompany(), result.getCompany());
+        assertEquals(mockEmployeeInputDto.getToHire(), result.getIsHired());
+    }
+
+    @Test
+    void xmlToJava() {
+        // Arrange
+
+        // Act
+
+        // Assert
+
+    }
+
+    @Test
+    void javaToJson() {
+        // Arrange
+
+        // Act
+
+        // Assert
+
     }
 }

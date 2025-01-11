7b64fa0fe726559c17daa6451222d3c5459728ae
Mark Mesander <mark_mesander@hotmail.com>
2025-01-11 15:36:56 +0100

    add method for java to json

--- src/main/java/nl/mesander/services/EmployeeService.java
+++ src/main/java/nl/mesander/services/EmployeeService.java
@@ -1,12 +1,13 @@
 package nl.mesander.services;
 
 // Imports
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.xml.bind.JAXBContext;
 import jakarta.xml.bind.JAXBException;
 import jakarta.xml.bind.Unmarshaller;
 import nl.mesander.dtos.input.EmployeeInputDto;
 import nl.mesander.dtos.output.EmployeeDto;
-
 import java.io.File;
 
 public class EmployeeService {
@@ -29,7 +30,7 @@ public class EmployeeService {
     // Methods
     public static EmployeeInputDto xmlToJava(String fileLocation) {
         try {
-            // Create file to make method work for different files
+            // Create file to make method universal
             File xmlFile = new File(fileLocation);
 
             // Create a new inputDto class
@@ -44,6 +45,21 @@ public class EmployeeService {
 
             return employeeInputDto;
         } catch (JAXBException error) {
+            error.printStackTrace();
+            return null;
+        }
+    }
+
+    public static String javaToJson(EmployeeDto employeeDto) {
+        try {
+            // Create an instance of objectmapper from jackson
+            ObjectMapper objectMapper = new ObjectMapper();
+
+            // Convert Java object to JSON string
+            String jsonString = objectMapper.writeValueAsString(employeeDto);
+
+            return jsonString;
+        } catch (JsonProcessingException error) {
             error.printStackTrace();
             return null;
         }
